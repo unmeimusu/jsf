f518590b36cf8450f08433ef9ae39ca3e934a11a
unmeimusu <unmeimusu@gmail.com>
2023-04-29 07:17:32 +0000

    Apr 29, 2023, 2:17 PM

---
+++ a/src/c/Add.svelte
@@ -0,0 +1,18 @@
+<script>
+    let arr = []
+    let num = 0
+
+    sum = num + num
+    
+</script>
+
+<input type="text" name="" id="" bind:value={num}/>
+<button on:click={sum}>Add</button>
+<div>
+    <ul>
+        <li>{num}</li>
+    </ul>
+</div>
+<div>
+    <p>{sum}</p>
+</div>
