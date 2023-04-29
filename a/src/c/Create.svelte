856311cc1db7bee9c9ba3c219274127841cb5b35
unmeimusu <unmeimusu@gmail.com>
2023-04-29 22:33:04 +0000

    Apr 30, 2023, 5:33 AM

--- a/src/c/Create.svelte
+++ a/src/c/Create.svelte
@@ -1,4 +1,11 @@
 <script>
+    let listname = []
+    let newname = ""
 
+    function add(newdata) {
+        listname.push(newdata)
+    }
 </script>
 
+<input type="text" bind:value={newname}>
+<button on:click={add(newname)}>Add</button>
