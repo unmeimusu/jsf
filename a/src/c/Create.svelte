3c69ddacb5aa4155b123f0a2f20422e304f70852
unmeimusu <unmeimusu@gmail.com>
2023-04-30 03:11:08 +0000

    Apr 30, 2023, 10:11 AM

--- a/src/c/Create.svelte
+++ a/src/c/Create.svelte
@@ -3,20 +3,23 @@
     let newname = ""
 
     function add() {
-        if (isNaN(newname)) {
+        if (isNaN(newname) || newname == "") {
             listname = listname
         } else {
             listname.push(newname)
             listname = listname // for array and object need to re-assign, telling the compiler it has changed
         }
+        newname = ""
     }
 </script>
 
 <h2>#3</h2>
 <input type="text" bind:value={newname}>
 <button on:click={add}>Add</button>
-<ul>
-    {#each listname as name,index}
-        <li>{index+1}. {name}</li>
-    {/each}
-</ul>
+<div>
+    <ul>
+        {#each listname as name,index}
+            <li>{index+1}. {name}</li>
+        {/each}
+    </ul>
+</div>
