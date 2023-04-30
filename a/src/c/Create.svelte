af1068c6d6705566c2fbe65d5f2c7f3f7af1857b
unmeimusu <unmeimusu@gmail.com>
2023-04-30 02:58:33 +0000

    Apr 30, 2023, 9:58 AM

--- a/src/c/Create.svelte
+++ a/src/c/Create.svelte
@@ -6,12 +6,12 @@
         listname.push(newname)
     }
 
-    $: x = add(newname)
+    $: x = add()
 </script>
 
 <h2>#3</h2>
 <input type="text" bind:value={newname}>
-<button on:click={add()}>Add</button>
+<button on:click={add}>Add</button>
 <ul>
     {#each listname as name,index}
         <li>{index+1}. {name}</li>
