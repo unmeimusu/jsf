b0c852aa2fe8b7716ca798a874199a9a121c5734
unmeimusu <unmeimusu@gmail.com>
2023-04-30 23:36:08 +0000

    May 1, 2023, 6:36 AM

--- a/src/c/Create.svelte
+++ a/src/c/Create.svelte
@@ -13,13 +13,13 @@
         newdata = ""
     }
 
-    function del(i) {
-        listdata.splice(i,1)
-    }
 
-    $: sum = listdata.reduce((total,num) => total+num,0)
-    $: del(i)
-    
+
+    $: { sum = listdata.reduce((total,num) => total+num,0)
+        function del(i) {
+            listdata.splice(i,1)
+        }
+    }
 </script>
 
 <h2>#3</h2>
