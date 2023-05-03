b193980ebea5c8677a371bf274d5b563fd4baada
unmeimusu <unmeimusu@gmail.com>
2023-05-03 13:30:44 +0700

    Update file

--- b/app.js
+++ b/app.js
@@ -1,24 +1,24 @@
 // reef object to call API methods
         // reef.store()
-        // same as with component and render
+        // #1 same as with component and render
         let {store, component, render} = reef;
 
-        // reactive data
+        // #2 use render
+        let name = 'world'
+        render("#app",`<p>Hello, ${name}!</p>`)
+
+        // #3 reactive data
         let data = store({
             greeting: 'Hello',
             name: 'World'
         });
-
-        // use render
-        let name = 'world'
-        render("#app2",`<p>Hello, ${name}!</p>`)
-
+        // data updated automatically
         data.greeting = 'Hi';
-        
+
         // or use a template function
         function template () {
             let {greeting, name} = data;
             return `<p>${greeting}, ${name}</p>`;
         };
         // after that use component to render template
-        component('#app', template);
+        component('#app2', template);
