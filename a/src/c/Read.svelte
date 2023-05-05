057755d6d9d1ce2289034cd38da914151faec439
unmeimusu <unmeimusu@gmail.com>
2023-05-05 07:22:59 +0000

    May 5, 2023, 2:22 PM

--- a/src/c/Read.svelte
+++ a/src/c/Read.svelte
@@ -1,5 +1,8 @@
 <script>
     let aNewTab = () => window.open()
+    let aCloseTab = () => window.close()
+    let moveTo = () => window.moveTo()
+    let resizeTo = () => window.resizeTo()
 </script>
 
 <style>
@@ -8,7 +11,9 @@
 <h3>Information Window:</h3>
 <p>Length: {Window.length}, Name: {Window.name}</p>
 <h3>Information Location:</h3><!--inconsistent ReferenceError: fixed by block comment it and then uncomment it again, or use event listener to call it-->
-<p>Host: {async () => {location.host }}, Hash: {async () => {location.hash}}, Hostname: {async () => {location.hostname}}, Origin: {async () => {location.origin}}, Port: {location.port}, Protocol: {location.protocol}, {async () => {location.pathname}}, {async () => {location.href}}</p>
+<!-- <p>Host: {async () => {location.host }}, Hash: {async () => {location.hash}}, Hostname: {async () => {location.hostname}}, Origin: {async () => {location.origin}}, Port: {location.port}, Protocol: {location.protocol}, {async () => {location.pathname}}, {async () => {location.href}}</p> -->
 <h3>Command methods</h3>
 <button on:click={aNewTab}>New Tab</button>
-<button on:click={aNewTab}>Open</button>
+<button on:click={aCloseTab}>Close</button>
+<button on:click={moveTo}>Move To</button>
+<button on:click={resizeTo}>Resize To</button>
