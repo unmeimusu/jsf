7b48aa559105ccfd232b7b43710549e093e79cf9
unmeimusu <unmeimusu@gmail.com>
2023-05-05 06:30:56 +0000

    May 5, 2023, 1:30 PM

--- a/src/c/Read.svelte
+++ a/src/c/Read.svelte
@@ -7,4 +7,4 @@
 <h3>Information Window:</h3>
 <p>Length: {Window.length}, Name: {Window.name}</p>
 <h3>Information Location:</h3><!--inconsistent ReferenceError: fixed by deactive to activate the code again-->
-<p>Host: {Location.host}, Hash: {location.hash}, Hostname: {location.hostname}, Origin: {location.origin}, Port: {location.port}, Protocol: {location.protocol}, {location.pathname}, {location.href}</p>
+<p>Host: {location.host}, Hash: {location.hash}, Hostname: {location.hostname}, Origin: {location.origin}, Port: {location.port}, Protocol: {location.protocol}, {location.pathname}, {location.href}</p>
