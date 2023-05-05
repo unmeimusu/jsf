b5ff2817c9ac3985e9d43cedaafed671952ef0aa
unmeimusu <unmeimusu@gmail.com>
2023-05-05 06:37:05 +0000

    May 5, 2023, 1:37 PM

--- a/src/c/Read.svelte
+++ a/src/c/Read.svelte
@@ -6,5 +6,7 @@
 
 <h3>Information Window:</h3>
 <p>Length: {Window.length}, Name: {Window.name}</p>
-<h3>Information Location:</h3><!--inconsistent ReferenceError: fixed by deactive to activate the code again-->
-<p>Host: {location.host}, Hash: {location.hash}, Hostname: {location.hostname}, Origin: {location.origin}, Port: {location.port}, Protocol: {location.protocol}, {location.pathname}, {location.href}</p>
+<h3>Information Location:</h3><!--inconsistent ReferenceError: fixed by block comment it and then unblock comment the code again-->
+<!-- <p>Host: {location.host}, Hash: {location.hash}, Hostname: {location.hostname}, Origin: {location.origin}, Port: {location.port}, Protocol: {location.protocol}, {location.pathname}, {location.href}</p>
+ --><h3>Command methods</h3>
+ <button on:click={window.open()}>Open</button>
