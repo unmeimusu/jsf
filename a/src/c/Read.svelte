10296acb037b0443af737ef03f8709faecd6a8cd
unmeimusu <unmeimusu@gmail.com>
2023-05-05 07:19:40 +0000

    May 5, 2023, 2:19 PM

--- a/src/c/Read.svelte
+++ a/src/c/Read.svelte
@@ -1,7 +1,5 @@
 <script>
-    function aNewTab() {
-        window.open()
-    }
+    let aNewTab = () => window.open()
 </script>
 
 <style>
@@ -12,4 +10,5 @@
 <h3>Information Location:</h3><!--inconsistent ReferenceError: fixed by block comment it and then uncomment it again, or use event listener to call it-->
 <p>Host: {async () => {location.host }}, Hash: {async () => {location.hash}}, Hostname: {async () => {location.hostname}}, Origin: {async () => {location.origin}}, Port: {location.port}, Protocol: {location.protocol}, {async () => {location.pathname}}, {async () => {location.href}}</p>
 <h3>Command methods</h3>
+<button on:click={aNewTab}>New Tab</button>
 <button on:click={aNewTab}>Open</button>
