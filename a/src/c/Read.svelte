b39bf71b577ca7c6bda335ef7a3a655b31b301cc
unmeimusu <unmeimusu@gmail.com>
2023-05-05 20:35:00 +0000

    May 6, 2023, 3:35 AM

--- a/src/c/Read.svelte
+++ a/src/c/Read.svelte
@@ -11,7 +11,7 @@
 <h3>Information Window:</h3>
 <p>Length: {Window.length}, Name: {Window.name}</p>
 <h3>Information Location:</h3><!--inconsistent ReferenceError: fixed by block comment it and then uncomment it again, or use event listener to call it-->
-<!-- <p>Host: {async () => {location.host }}, Hash: {async () => {location.hash}}, Hostname: {async () => {location.hostname}}, Origin: {async () => {location.origin}}, Port: {location.port}, Protocol: {location.protocol}, {async () => {location.pathname}}, {async () => {location.href}}</p> -->
+<p>Host: {window.location.host}}, Hash: {async () => {location.hash}}, Hostname: {async () => {location.hostname}}, Origin: {async () => {location.origin}}, Port: {location.port}, Protocol: {location.protocol}, {async () => {location.pathname}}, {async () => {location.href}}</p>-->
 <h3>Command methods</h3>
 <button on:click={aNewTab}>New Tab</button>
 <button on:click={aCloseTab}>Close</button>
